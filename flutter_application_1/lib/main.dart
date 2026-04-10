c177f42600daf977d2613fe63fd9ae551a71e2f0
ArvinSad <asad0025@student.monash.edu.au>
2026-04-10 23:28:16 +1000

    change seed color

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 89, 183, 58)),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
