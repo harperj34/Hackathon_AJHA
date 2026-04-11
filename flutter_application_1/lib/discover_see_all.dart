f080bffe811656fb251150f2e3be4e4a4daf25d5
ArvinSad <asad0025@student.monash.edu.au>
2026-04-11 12:07:28 +1000

    create see all page for discover

---
+++ flutter_application_1/lib/discover_see_all.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+ 
+class DiscoverSeeAll extends StatelessWidget {
+  const DiscoverSeeAll({super.key});
+ 
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Events Page',
+        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
